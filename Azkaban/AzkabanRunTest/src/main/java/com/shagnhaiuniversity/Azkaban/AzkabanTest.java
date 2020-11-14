20174d13874eb8f3c8c1dda70e0bc3a795773c65
xjl1995822 <18279148786@163.com>
2020-11-14 17:23:26 +0800

    Azkaban的测试代码部分
    Signed-off-by: xjl1995822 <18279148786@163.com>

--- Azkaban/AzkabanRunTest/src/main/java/com/shagnhaiuniversity/Azkaban/AzkabanTest.java
+++ Azkaban/AzkabanRunTest/src/main/java/com/shagnhaiuniversity/Azkaban/AzkabanTest.java
@@ -44,7 +44,8 @@ public class AzkabanTest {
 
     public void run() throws IOException {
         // 根据需求编写具体代码
-        FileOutputStream fos = new FileOutputStream("/opt/module/azkaban/output.txt");
+        String outpath = "";
+        FileOutputStream fos = new FileOutputStream(outpath);
         fos.write("this is a java progress".getBytes());
         fos.close();
     }
